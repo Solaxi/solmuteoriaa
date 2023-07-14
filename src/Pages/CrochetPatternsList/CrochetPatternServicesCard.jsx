bb7d10760634bc346d96277305b158d49f8a4f02
Solaxi <esko.pylsy@gmail.com>
2023-07-14 14:15:05 +0300

    Lazy loading pattern cards

---
+++ src/Pages/CrochetPatternsList/CrochetPatternServicesCard.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import Button from '../../components/ButtonElement'
+
+const Card = ({pattern}) => {
+  const [card, setCard] = useState([])
+
+  useEffect(() => {
+    import(`../../assets/crochet-patterns/${pattern}.json`)
+      .then(jsonData => {
+        setCard(jsonData)
+      })
+  }, [pattern])
+
+  const link = `/crochet-patterns/${pattern}`
+  return (
+    <div className='services__container__item card-hover'>
+      <div className='services__container__item__illustration'>
+        <img src={card.cardImg} alt={card.titleImgAlt} className='services__container__item__illustration__image' />
+      </div>
+      <div className='services__container__item__description'>
+        <h3 className='services__container__item__description__heading'>
+          {card && card.title}
+        </h3>
+        <p className='services__container__item__description__details'>
+          {card && card.cardDescription}
+        </p>
+        <Button to={link} title='See the pattern'/>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/Pages/CrochetPatternsList/CrochetPatternsServices.jsx
+++ src/Pages/CrochetPatternsList/CrochetPatternsServices.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import './CrochetPatternsServices.scss'
-import Button from '../../components/ButtonElement'
 
-import snowman from '/crochet-pattern-images/snowman/snowman.jpg'
-import mouse from '/crochet-pattern-images/mouse/mouse.jpg'
-import stella from '/crochet-pattern-images/stella/stella.jpg'
+const Card = lazy(() => import('./CrochetPatternServicesCard'))
+
+const patternsToShow = [
+  'snowman',
+  'mouse',
+  'angry-birds-stella'
+]
 
 function CrochetPatternServices() {
   return (
@@ -12,50 +15,9 @@ function CrochetPatternServices() {
       <h2 className='services__heading'>FREE CROCHET PATTERNS</h2>
       <div className='services__container'>
 
-        <div className='services__container__item card-hover'>
-          <div className='services__container__item__illustration'>
-            <img src={snowman} alt='Snowman' className='services__container__item__illustration__image' />
-          </div>
-          <div className='services__container__item__description'>
-            <h3 className='services__container__item__description__heading'>
-                Snowman
-            </h3>
-            <p className='services__container__item__description__details'>
-              I was asked to make the snowman from the movie The Snowman (1982). This is what I came up with.
-            </p>
-            <Button to='/crochet-patterns/snowman' title='See the pattern'/>
-          </div>
-        </div>
-
-        <div className='services__container__item card-hover'>
-          <div className='services__container__item__illustration'>
-            <img src={mouse} alt='Mouse' className='services__container__item__illustration__image' />
-          </div>
-          <div className='services__container__item__description'>
-            <h3 className='services__container__item__description__heading'>
-                Mouse
-            </h3>
-            <p className='services__container__item__description__details'>
-              So there I was, crocheting this cute little elephant... And I wondered what else is cute and has large ears. So I made a mouse.
-            </p>
-            <Button to='/crochet-patterns/mouse' title='See the pattern'/>
-          </div>
-        </div>
-
-        <div className='services__container__item card-hover'>
-          <div className='services__container__item__illustration'>
-            <img src={stella} alt='Angry Birds Stella' className='services__container__item__illustration__image' />
-          </div>
-          <div className='services__container__item__description'>
-            <h3 className='services__container__item__description__heading'>
-                Angry Birds Stella
-            </h3>
-            <p className='services__container__item__description__details'>
-              Having crochet all the other Angry Birds characters, I got a request to do Stella. As that pattern didn't exist, I made one.
-            </p>
-            <Button to='/crochet-patterns/angry-birds-stella' title='See the pattern'/>
-          </div>
-        </div>
+        {patternsToShow.map(p =>  
+          <Card key={p} pattern={p} />
+        )}
 
       </div>
     </section>
