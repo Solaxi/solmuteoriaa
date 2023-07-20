1054e2dd5cd567f9311db23b63236b867159fb69
Solaxi <esko.pylsy@gmail.com>
2023-07-20 12:15:25 +0300

    ENV config for seo links

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet-async'
 
 function SEO({title, description, image}) {
-  const ogImage = `${window.location.protocol}//${window.location.hostname}${image}`
-
+  const host = import.meta.env.VITE_HOST_URL
   return (
     <Helmet>
       <title>Solmuteoriaa - {title}</title>
@@ -13,12 +12,12 @@ function SEO({title, description, image}) {
       <meta property='og:url' content={window.location.href}/>
       <meta property='og:title' content={title}/>
       <meta property='og:description' content={description}/>
-      <meta property='og:image' content={ogImage}/>
+      <meta property='og:image' content={`${host}${image}`}/>
 
       <meta name='twitter:card' content='summary_large_image'/>
       <meta name='twitter:title' content={title}/>
       <meta name='twitter:description' content={description}/>
-      <meta name='twitter:image' content={ogImage}/>
+      <meta name='twitter:image' content={`${host}${image}`}/>
       <meta name='twitter:creator' content='@EskoPoutanen' />
       <meta name='twitter:site' content='@EskoPoutanen' />
     </Helmet>
