da461a21b55c5912e31616e0de8339f8da2a9db9
Solaxi <esko.pylsy@gmail.com>
2023-07-29 17:37:54 +0300

    Added placeholder=blur to images

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const ImageElement = ({img, alt, classname}) => 
-  <img src={`/images${img}`} alt={alt || ''} className={classname || ''} />
+  <img src={`/images${img}`} alt={alt || ''} className={classname || ''} placeholder='blur' />
 
 export default ImageElement
