8754f9386448bd8595d0bdf5c7c58c2b8846b34e
Solaxi <esko.pylsy@gmail.com>
2023-07-29 11:07:53 +0300

    Added 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import '../styles/default-page.scss'
+import '../styles/404.scss'
+import Layout from '../components/layout'
+import SEO from '../components/SEO'
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>
-        Oh no 404!
-      </h1>
-      </main>
+    <Layout>
+    <div className='page'>
+      <div className='page__header padding'>
+        <h1 className='page__header__heading'>404</h1>
+      </div>
+      <div className='page__body'>
+        <section className='content404 padding'>
+          This page does not exist.
+        </section>
+      </div>
+    </div>
+    </Layout>
   )
 }
 
 export default NotFoundPage
 
-export const Head = () => <title>Not Found Page</title>
+export const Head = () => 
+<SEO 
+  title='Solmuteoriaa - Page Not Found'
+  description='This page does not exist. Sorry.'
+  pathname='/404'
+/>
